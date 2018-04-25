f951147e7b0922eed10892aa11a953110cf9b49d
Nicolas Franck <nicolas.franck@ugent.be>
2018-04-25 11:16:54 +0200

    0.1254

--- lib/Activiti/Rest/Client.pm
+++ lib/Activiti/Rest/Client.pm
@@ -7,7 +7,7 @@ use JSON qw(decode_json encode_json);
 use URI::Escape qw(uri_escape);
 use Activiti::Rest::Response;
 
-our $VERSION = "0.1253";
+our $VERSION = "0.1254";
 
 #see: http://www.activiti.org/userguide
 
